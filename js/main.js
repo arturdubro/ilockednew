46a8e9b01d13950276cb74d390e3e46f9031926e
Artur Dubro <arturdubro@gmail.com>
2014-05-16 00:56:00 +0400

    14.1.2.3

--- js/main.js
+++ js/main.js
@@ -56,7 +56,10 @@ function onPlayerStateChange(event) {
 $(document).ready(function(){
     $(document).on('click', '.playvideo', function(event){
     	event.preventDefault();
-        if (!isMobile.any() && (video.getPlayerState() == 0 || video.getPlayerState() == 2)) video.playVideo();
+        if (!isMobile.any()) { 
+            video.playVideo();
+            alert('playing!');
+        };
         $('.main-banner').hide();
         $('#video').show();
     });
