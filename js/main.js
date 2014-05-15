e838a8e7b934e19d839e22ca73ecf621d0e025f7
Artur Dubro <arturdubro@gmail.com>
2014-05-15 10:01:43 +0400

    11

--- js/main.js
+++ js/main.js
@@ -13,10 +13,46 @@ function startGallery() {
     	if (currentstate == pirates.length) currentstate = 0;
     	var imgsrc = pirates[currentstate];
     	};
-	$('.'+quest).css('background-image','url('+imgsrc+')');
+	//$('.'+quest).css('background-image','url('+imgsrc+')');
+	$('.'+quest).animate({opacity: 0}, 'slow', function() {
+		$(this).css('background-image','url('+imgsrc+')').animate({opacity: 1});
+	});
 	currentstate++;
 	setTimeout(startGallery, 7200);
     };
 };
 startGallery();
+});
+
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+function onYouTubeIframeAPIReady() {
+  video = new YT.Player( 'video', {
+  width: '1000px',
+  height: '550px',
+  videoId: 'aqQ7Hv8YL6k',
+  playerVars: { 'controls': 0, 'showinfo': 0 },
+  events: {
+    'onStateChange': onPlayerStateChange
+  }
+  });
+};
+
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.ENDED || event.data == YT.PlayerState.PAUSED) {
+    if (video.getPlayerState() == 0 || video.getPlayerState() == 2) $('#video').hide();
+    $('.main-banner').show();
+    };
+};
+
+$(document).ready(function(){
+    $(document).on('click', '.content-main', function(event){
+    	event.preventDefault();
+        video.playVideo();
+        $('.main-banner').hide();
+        $('#video').show();
+    });
 });
